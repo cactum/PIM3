5962ca02c935e5c2936667ce3c1167d5f9e49f90
Cactum <andi.arrudaalves@gmail.com>
2023-10-25 17:16:00 -0300

    add tela login

---
+++ PIM3/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PIM3.Models;
+
+namespace PIM3.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Entrar(LoginModel loginModel)
+        {
+            try
+            {
+                if (@ModelState.IsValid)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return View("Index");
+            }
+            catch (Exception erro)
+            {
+
+                TempData["MensagemErro"] = $"Algo saiu errado, não foi possivel logar, tente novamente! \n" +
+                   $"detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
